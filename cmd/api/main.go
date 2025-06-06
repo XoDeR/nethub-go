9c402d59441da335d8e165c6876970ed3cfbb2f8
XoDeR <xoder.vs@gmail.com>
2025-06-06 22:29:00 +0200

    added logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+)
 
 const version = "1.0.0"
 
 func main() {
+	// test basic output
 	fmt.Println("NetHub version: " + version)
+
+	// load config from env vars
+
+	// enable logger
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	defer logger.Sync()
+
+	// test logger
+	logger.Info("NetHub", zap.String("status", "running"))
 }
