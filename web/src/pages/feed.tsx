91c9f373925a85ac0bca9c31c7b9261155a15524
XoDeR <xoder.vs@gmail.com>
2025-06-24 19:53:09 +0200

    added pagination component

--- web/src/pages/feed.tsx
+++ web/src/pages/feed.tsx
@@ -1,2 +1,32 @@
-const Feed: React.FC = () => <h1>Protected Feed Page</h1>;
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+const Feed: React.FC = () => {
+  return <div>
+    <h1>Protected Feed Page</h1>
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  </div>
+}
 export default Feed;
